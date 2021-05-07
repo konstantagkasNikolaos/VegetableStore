9a37dd998612dc489236d872f821e0d42e1dcf2e
konstantagkas <konstantagkas.nikolaos@gmail.com>
2021-05-07 23:32:26 +0300

    Vegetable Store

---
+++ src/main/java/vegetableStore/VegetableStoreApplication.java
@@ -0,0 +1,13 @@
+package vegetableStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetableStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegetableStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vegetableStore/VegetableStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vegetableStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegetableStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
